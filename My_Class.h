e1420e05aa8bd3c630218143728568ef1e40db3c
yanjun <liushouyun@163.com>
2016-08-22 16:58:59 +0800

    add heap class

--- My_Class.h
+++ My_Class.h
@@ -11,16 +11,89 @@ struct Interval {
     Interval() : start(0), end(0) {}
     Interval(int s, int e) : start(s), end(e) {}
 };
-class heep{
+class max_heap{
 private:
     int* data;
     int n;
+    int size;
+    void swap(int* a,int* b){
+        int buff=*a;
+        *a=*b;
+        *b=buff;
+    }
+    void max_heapify(int start){
+        int dad=start;
+        int son=dad*2+1;
+        while(son<n){
+            if(son+1<n)son=data[son]>data[son+1]?son:son+1;
+            if(data[son]>data[dad])
+            {
+                swap(data+son,data+dad);
+                dad=son;
+                son=dad*2+1;
+            }
+            else return;
+        }
+    }
 public:
-    heep():data(NULL),n(0){};
-    heep(int* data,int n){
-
+    void heap_sort(int A[],int n){
+        create_heap(A,n);
+        while(this->n>0){
+            heap_delete();
+        }
+    }
+    int heap_get(){
+        if(data==NULL)return -1;
+        else if(n==0) return -1;
+        else return data[0];
     }
 
+    bool heap_delete(){
+        if(data==NULL)return false;
+        else{
+            swap(data,data+n-1);
+            n--;
+            max_heapify(0);
+            return true;
+        }
+    }
+    bool heap_insert(int a){
+        if(this->size==this->n){
+            return false;
+        }
+        else{
+            this->data[this->n]=a;
+            this->n++;
+            int son=this->n-1;
+            int dad=(son-1)/2;
+            while(dad>=0){
+                if(data[son]>data[dad]){
+                    swap(data+son,data+dad);
+                    son=dad;
+                    dad=(son-1)/2;
+                }
+                else return true;
+            }
+            return true;
+        }
+    }
+    max_heap():data(NULL),n(0),size(0){};
+    void create_heap(int* data,int n,int size){
+        this->data=data;
+        this->n=n;
+        this->size=size;
+        for(int i=n/2-1;i>=0;i--){
+            max_heapify(i);
+        }
+    }
+    void create_heap(int* data,int n){
+        this->data=data;
+        this->n=n;
+        this->size=n;
+        for(int i=n/2-1;i>=0;i--){
+            max_heapify(i);
+        }
+    }
 };
 
 struct binary_tree{
--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "HUAWEI.h"
 #include "LEETCODE.h"
 #include "COURSE.h"
+#include "My_Class.h"
 
 using namespace std;
 
@@ -17,7 +18,66 @@ int main() {
     huawei myhuawei;
     sort mysort;
     divide_conquer mydivide_conquer;
+    max_heap my_max_heap;
+
     int n;
+    int* data;
+    int state;
+
+    while(cin>>n){
+        int* A=new int[n];
+        for(int i=0;i<n;i++){
+            cin>>A[i];
+        }
+        my_max_heap.heap_sort(A,n);
+        for(int i=0;i<n;i++){
+            cout<<A[i]<<" ";
+        }
+        cout<<endl;
+        delete[] A;
+    }
+
+    while(1){
+        cout<<"input heap size:";
+        cin>>n;
+        data=new int[n];
+        state=-1;
+        while(!(state==1||state==2)) {
+            cout << "create NULL heap:1\ncreate heap by list:2" << endl;
+            cin >> state;
+            if (state == 1) {
+                my_max_heap.create_heap(data,0,n);
+            }
+            else if(state==2) {
+                for(int i=0;i<n;i++){
+                    cin>>*(data+i);
+                }
+                my_max_heap.create_heap(data,n);
+            }
+        }
+        state=-1;
+        while(state!=4){
+            cout<<"insert:1\ndelete:2\nget:3\nquit:4"<<endl;
+            cin>>state;
+            switch (state){
+                case 1:
+                    int a;
+                    cin>>a;
+                    cout<<my_max_heap.heap_insert(a)<<endl;
+                    break;
+                case 2:
+                    cout<<my_max_heap.heap_delete()<<endl;
+                    break;
+                case 3:
+                    cout<<my_max_heap.heap_get()<<endl;
+                    break;
+            }
+        }
+        delete[] data;
+    }
+
+
+
     while(cin>>n){
         int* A=new int[n];
         for(int i=0;i<n;i++){
@@ -27,18 +87,6 @@ int main() {
         delete[] A;
     }
 
-    while(cin>>n){
-        int* A=new int[n];
-        for(int i=0;i<n;i++){
-            cin>>A[i];
-        }
-        mysort.merge(A,n);
-        for(int i=0;i<n;i++){
-            cout<<A[i]<<" ";
-        }
-        cout<<endl;
-        delete[] A;
-    }
 
     while(cin>>n){
         for(int i=0;i<n;i++){
